7b23bb7db8c2abb078d2a39fef6723a7d74bf8c7
roman2013 <romanyu827@gmail.com>
2015-07-15 12:58:47 +0800

    Initial Commit

---
+++ CoreGraphicsTryer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreGraphicsTryer
+//
+//  Created by Chen YU on 15/7/15.
+//  Copyright (c) 2015 Chen YU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreGraphicsTryer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CoreGraphicsTryer
+//
+//  Created by Chen YU on 15/7/15.
+//  Copyright (c) 2015 Chen YU. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
