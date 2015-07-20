f581b91ccdf06a16e839f4c3a6d07aabd081a321
roman2013 <romanyu827@gmail.com>
2015-07-20 19:56:57 +0800

    Draw seats with different colors.

--- CoreGraphicsTryer/Components/BPRTCircleTable.m
+++ CoreGraphicsTryer/Components/BPRTCircleTable.m
@@ -14,7 +14,6 @@
 @property (nonatomic, assign) NSInteger numberOfSeats;
 
 @property (nonatomic, strong) UIImageView *seatImageView;
-@property (nonatomic, strong) NSMutableArray *seats;
 
 @end
 
@@ -25,20 +24,13 @@
         self.backgroundColor = [UIColor clearColor];
         self.numberOfSeats = 8;
         
-        _seatImageView = [[UIImageView alloc] initWithFrame:frame];
-//        [self addSubview:_seatImageView];
+        _seatImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
+        [self addSubview:_seatImageView];
     }
     return self;
 }
 
 #pragma mark - Getter
-
-- (NSMutableArray *)seats {
-    if (!_seats) {
-        _seats = [NSMutableArray new];
-    }
-    return _seats;
-}
 
 - (void)layoutSubviews {
     [super layoutSubviews];
@@ -46,50 +38,31 @@
 
 - (void)drawSeats {
     for (int i = 0; i < self.numberOfSeats; i++) {
-        [self drawSeatImageView:(360 / self.numberOfSeats * i)];
+        UIColor *color = [@[[UIColor redColor], [UIColor greenColor], [UIColor blueColor], [UIColor purpleColor]] objectAtIndex:(arc4random() % 4)];
+        [self drawSeatImageView:(360 / self.numberOfSeats * i) withColor:color];
     }
+    
+    UIGraphicsBeginImageContext(self.seatImageView.frame.size);
+    [self.seatImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    self.seatImageView.image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    [self.seatImageView.subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
 }
 
-- (void)drawSeatWithDegree:(CGFloat)degreeToRotate {
+- (void)drawSeatImageView:(CGFloat)degreeToRotate withColor:(UIColor *)color {
     
-    for (int i = 0; i < 2; i++) {
-        UIRectCorner corner = UIRectCornerAllCorners;
-        CGPoint origin = CGPointZero;
-        if (i == 0) {
-            corner = UIRectCornerTopLeft | UIRectCornerTopRight;
-            origin = CGPointMake(self.bounds.size.width / 2 - kSeatWidth / 2, 0);
-        } else {
-            corner = UIRectCornerBottomLeft | UIRectCornerBottomRight;
-            origin = CGPointMake(self.bounds.size.width / 2 - kSeatWidth / 2, self.bounds.size.height - kSeatHeight);
-        }
-        
-        UIBezierPath* path = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(origin.x, origin.y, kSeatWidth, kSeatHeight)
-                                                   byRoundingCorners:corner
-                                                         cornerRadii:CGSizeMake(5.0, 5.0)];
-        
-        degreeToRotate = M_PI * degreeToRotate / 180;
-        CGAffineTransform transform = CGAffineTransformMakeRotation(degreeToRotate);
-        [path applyTransform: transform];
-        
-        [[UIColor colorWithRed:224.0 / 255.0 green:231.0 / 255.0 blue:239.0 / 255.0 alpha:1.0] setFill];
-        [path fill];
-    }
-}
-
-- (void)drawSeatImageView:(CGFloat)degreeToRotate {
-    
-    UIImageView *tempImageView = [[UIImageView alloc] initWithFrame:CGRectMake(self.bounds.size.width / 2 - kSeatWidth / 2, 0, kSeatWidth, self.bounds.size.height)];
+    UIImageView *tempImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.bounds.size.width, self.bounds.size.height)];
     
     UIGraphicsBeginImageContext(tempImageView.frame.size);
     
-//    [self.seatImageView.image drawInRect:self.seatImageView.frame blendMode:kCGBlendModeNormal alpha:1.0];
     [tempImageView.image drawInRect:tempImageView.frame blendMode:kCGBlendModeNormal alpha:1.0];
     
-    UIBezierPath* path = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(0, 0, kSeatWidth, kSeatHeight)
+    UIBezierPath* path = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(self.bounds.size.width / 2 - kSeatWidth / 2, 0, kSeatWidth, kSeatHeight)
                                                byRoundingCorners:UIRectCornerTopLeft | UIRectCornerTopRight
                                                      cornerRadii:CGSizeMake(5.0, 5.0)];
     
-    [[UIColor blackColor] setFill];
+    [color setFill];
     [path fill];
     
     // 5
@@ -100,8 +73,7 @@
     CGAffineTransform transform = CGAffineTransformMakeRotation(angle);
     tempImageView.transform = transform;
     
-    [self addSubview:tempImageView];
-    [self.seats addObject:tempImageView];
+    [self.seatImageView addSubview:tempImageView];
 }
 
 // Only override drawRect: if you perform custom drawing.
@@ -119,9 +91,6 @@
         [path fill];
         
         [self drawSeats];
-        
-//        [self drawSeatImageView: 0];
-//        [self drawSeatImageView: 90];
     }
 }
 
